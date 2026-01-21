7f0ae340cce90b206b96d23c6cb420acea208359
Tiago Pinheiro <tiago.oliveira.pinheiro@gmail.com>
2026-01-21 15:06:53 +0000

    Cria controllers, models, migrations, api e routes para o projeto

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\InvoiceController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('user', UserController::class);
+Route::apiResource('client', ClientController::class);
+Route::apiResource('task', TaskController::class);
+Route::apiResource('invoice', InvoiceController::class);
