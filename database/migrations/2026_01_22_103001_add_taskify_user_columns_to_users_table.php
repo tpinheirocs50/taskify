14001f7a760737c6d804116d5ca8ddd33447c0fa
Tiago Pinheiro <tiago.oliveira.pinheiro@gmail.com>
2026-01-23 15:12:02 +0000

    Adiciona restantes colunas as tabelas users e invoices

---
+++ database/migrations/2026_01_22_103001_add_taskify_user_columns_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('tin');
+            $table->text('address');
+            $table->string('phone')->nullable();
+            $table->jsonb('appearance')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'tin',
+                'address',
+                'phone',
+                'appearance',
+            ]);
+        });
+    }
+};
---
+++ database/migrations/2026_01_22_111600_add_taskify_invoice_columns_to_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->date('date')->default(DB::raw('CURRENT_DATE'));
+            $table->enum('status', ['draft','sent','paid','overdue','cancelled'])->default('draft');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropColumn([
+                'date',
+                'status',
+            ]);
+        });
+    }
+};
