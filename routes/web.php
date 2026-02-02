a4303c12bfb37cc0d74c0ffa7a1cce9aabecc226
Tiago Pinheiro <tiago.oliveira.pinheiro@gmail.com>
2026-02-02 14:05:13 +0000

    Adiciona pagina de calendario mensal

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,36 @@
 <?php
- 
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
- 
+
 Route::get('/', function () {
     return Inertia::render('welcome', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
- 
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
- 
+
     Route::get('tasks', function () {
         return Inertia::render('tasks/index');
     })->name('tasks');
- 
+
     Route::get('clients', function () {
         return Inertia::render('clients/index');
     })->name('clients');
- 
+
     Route::get('invoices', function () {
         return Inertia::render('invoices/index');
     })->name('invoices');
+
+    Route::get('calendar', function () {
+        return Inertia::render('calendar/index');
+    })->name('calendar');
 });
- 
- 
-require __DIR__.'/settings.php';
+
+
+require __DIR__ . '/settings.php';
